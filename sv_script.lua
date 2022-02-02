f2b9de4f5d53243039d0e47f743bcb0d0d434888
Denis <smoke8748@gmail.com>
2022-02-03 00:38:18 +0900

    Create sv_script.lua

---
+++ sv_script.lua
@@ -0,0 +1,11 @@
+//Оружия которые надо запретить
+local weapon = {
+    ['weapon_357'] = true
+}
+//Работа которой мы запрещаем перечисленные выше оружия
+local job = {
+    [TEAM_CITIZEN] = true
+}
+hook.Add("PlayerCanPickupWeapon", "Proverka", function(ply, wep)
+    if (weapon[wep:GetClass()] && job[ply:Team()]) then DarkRP.notify(ply, 1, 2,"Вам запрещено брать данное оружие!") return false end
+end)
